6fe60d5671a58a1a31df7a4100f85391c72c9d26
boowerk <back20044@gmail.com>
2024-09-30 19:11:29 +0900

    ESC 키와 윈도우창 x 키 누르면 종료 구현 완료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,6 +6,16 @@ character = load_image('sprite_sheet.png')
 background = load_image('TUK_GROUND.png')
 
 def handle_events():
+
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
     pass
 
 running = True
