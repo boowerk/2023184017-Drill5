b33e4b3f974cd2fe3f1150f57db3fdf9e5a6e3f2
boowerk <back20044@gmail.com>
2024-09-30 19:21:29 +0900

    w 키 구현 완료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,7 +7,7 @@ background = load_image('TUK_GROUND.png')
 
 def handle_events():
 
-    global running
+    global running, dir, move_x, move_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -15,6 +15,13 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
+            elif event.key == SDLK_w:
+                move_y += 5
+                dir = 'up'
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_w:
+                move_y -= 5
+
 
     pass
 
@@ -24,15 +31,23 @@ sprite_height = 141
 frame = 0
 dir = 'down'
 x, y = 400, 300
-dir_rows = {'left' : 0, 'down' : 1, 'right' : 2, 'up' : 3}
+move_x, move_y = 0, 0
+dir_rows = {'down' : 0, 'left' : 1, 'right' : 2, 'up' : 3}
 
 while running:
     clear_canvas()
     background.clip_draw(0, 0, 1280, 1080, 400, 300, 800, 600)
     current_row = dir_rows[dir]
     character.clip_draw(frame * sprite_width, (3 - current_row) * sprite_height, sprite_width, sprite_height, x, y)
+
+    x += move_x
+    y += move_y
+
     update_canvas()
     handle_events()
+
+    frame = (frame + 1) % 3
+    delay(0.1)
     pass
 
 
